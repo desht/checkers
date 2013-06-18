44b4596f844128d40c40eeb71584c6dfafbbc27a
Des Herriott <des.herriott@gmail.com>
2013-06-18 09:12:19 +0100

    Version => 1.0.1

--- src/main/java/me/desht/checkers/view/BoardView.java
+++ src/main/java/me/desht/checkers/view/BoardView.java
@@ -2,7 +2,6 @@ package me.desht.checkers.view;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
